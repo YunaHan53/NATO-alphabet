1056043b6f0031ce75c0864a15b832d56c38f6a5
YunaH <Yunahan53@gmail.com>
2026-02-09 16:15:16 -0500

    Added exception handling to catch any KeyErrors and converted it into a function.

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# print(data.to_dict())
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
+print(phonetic_dict)
 
+def generate_phonetic():
+    user_word = input("Please enter a word: ").upper()
+    try:
+        result = [phonetic_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters A-Z are allowed. Try again.")
+        generate_phonetic()
+    else:
+        print(result)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Please enter a word: ").upper()
-result = [phonetic_dict[letter] for letter in user_word]
-# user_letters = list(user_word)
-
-# user_letter_code = {}
-# for (index, row) in data.iterrows():
-#     if row.letter in user_letters:
-#         user_letter_code[row.letter] = row.code
-
-# user_letter_code = {row.letter: row.code for (index, row) in data.iterrows() if row.letter in user_letters}
-print(result)
+generate_phonetic()
